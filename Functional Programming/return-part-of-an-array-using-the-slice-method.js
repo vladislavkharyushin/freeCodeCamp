a846eeedac745008ed93a222339fa7f267419ac5
Vladislav <vx3110@yandex.ru>
2023-10-13 12:42:43 +0300

    Project added

---
+++ Functional Programming/return-part-of-an-array-using-the-slice-method.js
@@ -0,0 +1,12 @@
+//DESCRIPTION:
+//Use the slice method in the sliceArray function to return part of the anim 
+//array given the provided beginSlice and endSlice indices. The function should return an array.
+
+//SOLUTION:
+
+function sliceArray(anim, beginSlice, endSlice) {
+ return anim.slice(beginSlice, endSlice);
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
